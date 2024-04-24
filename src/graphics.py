87a53f74e7a97db574c4165de4d142a5384aef1f
kasualkid12 <pdwalker3@gmail.com>
2024-04-24 16:37:20 -0500

    added draw functionality

--- src/graphics.py
+++ src/graphics.py
@@ -1,7 +1,7 @@
 from tkinter import Tk, BOTH, Canvas
 
 class Window:
-  def __init__(self, width, height, title="My Window") -> None:
+  def __init__(self, width, height, title="Maze Solver") -> None:
     # Initialize the root widget
     self.root = Tk()
     self.root.title(title) # Set the window title
@@ -22,9 +22,29 @@ class Window:
 
   def wait_for_close(self):
     self.is_running = True
-
     while self.is_running == True:
       self.redraw()
+    print("window closed...")
   
+  def draw_line(self, line, fill_color="black"):
+    line.draw(self.canvas, fill_color)
+
   def close(self):
     self.is_running = False
+
+
+class Point:
+  def __init__(self, x, y) -> None:
+    self.x = x
+    self.y = y
+
+
+class Line:
+  def __init__(self, point1, point2) -> None:
+    self.point1 = point1
+    self.point2 = point2
+
+  def draw(self, canvas, fill_color="black"):
+    canvas.create_line(
+    self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2
+)
