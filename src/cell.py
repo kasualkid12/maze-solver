94d2c38e38d4ccdf4ef3f7008f2242a672d46a9c
kasualkid12 <pdwalker3@gmail.com>
2024-04-25 12:08:26 -0500

    added draw_move

--- src/cell.py
+++ src/cell.py
@@ -1,18 +1,18 @@
 from graphics import Line, Point
 
 class Cell:
-  def __init__(self, win) -> None:
-    self.has_left_wall = True
-    self.has_right_wall = True
-    self.has_top_wall = True
-    self.has_bottom_wall = True
-    self._x1 = None
-    self._x2 = None
-    self._y1 = None
-    self._y2 = None
-    self._win = win
+    def __init__(self, win) -> None:
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
 
-  def draw(self, x1, y1, x2, y2):
+    def draw(self, x1, y1, x2, y2):
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
@@ -29,3 +29,19 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        line_color = "red"
+        if undo:
+            line_color = "grey"
+
+        self_center = self.find_center()
+        to_cell_center = to_cell.find_center()
+
+        line = Line(self_center, to_cell_center)
+        self._win.draw_line(line, line_color)
+
+    def find_center(self):
+        x = (self._x1 + self._x2) / 2
+        y = (self._y1 + self._y2) / 2
+        return Point(x, y)
